72a63d8fd36979b3b368aeb9e6ab40d55577da62
karim <karim1430@gmail.com>
2024-09-20 17:34:20 +0300

    create splash view  and splash view body and theme

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/constant.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: SplashView(),
+    );
+  }
+}
