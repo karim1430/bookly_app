c07c61c444a4e1a624746ee8cf7112fc33a1b3c9
karim <karim1430@gmail.com>
2024-09-24 12:15:41 +0300

    done  fetchNewestBooks and fetchFeatureBooks

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/erorrs/failure.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -20,14 +21,38 @@ class HomeRepoImpl implements HomeRepo {
         books.add(BookModel.fromJson(book));
       }
       return right(books);
-    } on Exception catch (e) {
-      return left(ServiceFailure());
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServiceFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServiceFailure(e.toString()),
+      );
     }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServiceFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServiceFailure(e.toString()),
+      );
+    }
   }
 }
--- lib/core/erorrs/failure.dart
+++ lib/core/erorrs/failure.dart
@@ -1,3 +1,46 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServiceFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServiceFailure extends Failure {
+  ServiceFailure(super.errMessage);
+
+  factory ServiceFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServiceFailure('Connection timout with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServiceFailure('Send timout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServiceFailure('Receive timout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServiceFailure('bad Certificate timout with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServiceFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response);
+      case DioExceptionType.cancel:
+        return ServiceFailure('Request to ApiServer Canceld');
+      case DioExceptionType.connectionError:
+        return ServiceFailure('No Intenet Connection');
+      case DioExceptionType.unknown:
+        return ServiceFailure(dioError.error.toString());
+    }
+  }
+
+  factory ServiceFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 402) {
+      return ServiceFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServiceFailure('your request not found , please try later ');
+    } else if (statusCode == 500) {
+      return ServiceFailure('Internal Server error , please try later');
+    } else {
+      return ServiceFailure('Oops there was an error , please try later');
+    }
+  }
+}
